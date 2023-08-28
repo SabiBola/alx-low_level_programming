f7d9ca0fc60f8ee7458ab8e26417823950970b08
sabbbeee <sabi.vibess@gmail.com>
2023-08-28 18:55:47 +0100

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,31 @@
-7-print_chessboard.c
+#include <stdio.h>
 
-#include "main.h"
-/**
- * print_chessboard - Entry point
- * @a: array
- * Return: Always 0 (Success)
- */
 void print_chessboard(char (*a)[8])
 {
-	int i;
-	int j;
-
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-		_putchar('\n');
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			printf("%c ", a[i][j]);
+		}
+		printf("\n");
 	}
+}
+
+int main()
+{
+	// Define a chessboard as a 2D array of characters
+	char chessboard[8][8] = {
+		{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+		{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+	};
+
+	printf("Chessboard:\n");
+	print_chessboard(chessboard);
+
+	return 0;
 }
